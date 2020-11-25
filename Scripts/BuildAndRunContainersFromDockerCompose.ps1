0f190f064b00f4eed8123d24653daab40e65e9d8
zapryan.bekirski <zbekirski@abv.bg>
2020-11-25 22:55:12 +0200

    Initial "implementation" of the docker-compose file.

---
+++ Scripts/BuildAndRunContainersFromDockerCompose.ps1
@@ -0,0 +1,5 @@
+﻿###!!!RUN THIS SCRIPT IN THE DIRECTORY CONTAINING THE docker-compose file.
+#build images in docker-compose file
+docker-compose build
+#run containers from docker-compose file. -d stands for discrete - it reduces the logs in the terminal
+docker-compose up -d
