e1b8baec02cad9f9b31bc1c37abccdf59be7e402
zapryan.bekirski <zbekirski@abv.bg>
2020-12-02 20:10:53 +0200

    Moved the environment variables to ConfigMap and Secret kubernetes objects.

--- Scripts/Kubernetes/ClearLocalKubernetesConfig.ps1
+++ Scripts/Kubernetes/ClearLocalKubernetesConfig.ps1
@@ -1,3 +1,4 @@
-﻿kubectl delete -f ..\..\.k8s\Database
+﻿kubectl delete -f ..\..\.k8s\Environment
+kubectl delete -f ..\..\.k8s\Database
 kubectl delete -f ..\..\.k8s\EventBus
 kubectl delete -f ..\..\.k8s\API
